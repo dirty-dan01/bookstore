c1a12523da2bc2d02bbfe83cde29883badab7941
daniel <guilledaniel_1@hotmail.com>
2024-04-16 11:19:12 -0600

    Setup commit

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://guilledaniel22:IYFBtqzdfOR9pYyM@cluster0.hs4qous.mongodb.net/books-colletion?retryWrites=true&w=majority&appName=Cluster0'
