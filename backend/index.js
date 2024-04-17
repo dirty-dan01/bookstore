8d0c70da658e89a7a037c0828881dacdc631e31a
dirty-dan01 <guilledaniel_1@hotmail.com>
2024-04-17 13:13:44 -0600

    Front end setup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
 import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import booksRoute from "backend/routes/bookRoutes.js"
-
+import booksRoute from "./routes/booksRoute.js";
+import cors from 'cors';
 const app = express();
 
+//middleware for parsing request body
 app.use(express.json());
+
+//Middleware for handling CORS POLICY
+//allow all originswith defaultof cors(*)
+//app.use(cors());
+//allow custom origins
+app.use(cors({
+    origin:'http://localhost:3000',
+    methods:['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders:['Content-Type'],
+}));
+
 
 app.get('/', (request, response)=>{
     console.log(request)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-600 text-white'>
+      Texto
+    </div>
+  )
+}
+
+export default App
+
+
+
