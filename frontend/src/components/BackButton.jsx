f1fa3a0f2a6c9f8c15f91f4657de59bfe0e5aa66
dirty-dan01 <guilledaniel_1@hotmail.com>
2024-04-17 18:18:25 -0600

    BackButton Component

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+import { BsArrowLeft } from "react-icons/bs";
+
+const BackButton = () => {
+  return <div>BackButton</div>;
+};
+
+export default BackButton;
