ff2ba41eacd285f5517d20c3c4186572f40c6830
dirty-dan01 <guilledaniel_1@hotmail.com>
2024-04-22 15:26:10 -0600

    Final refactor

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -9,33 +9,37 @@ import { useNavigate, useParams } from "react-router-dom";
 const DeleteBook = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const {id} = useParams();
+  const { id } = useParams();
   const handleDeleteBook = () => {
-    setLoading(true); 
-    axios.delete(`http://localhost:5555/books/${id}`)
-    .then(() => {
-      setLoading(false);
-      navigate('/');
-    })
-    .catch((error) => {
-      setLoading(false);
-      alert('An error has happened. Check console')
-      console.log(error);
-    })
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("An error has happened. Check console");
+        console.log(error);
+      });
   };
   return (
     <div className="p-4">
-      <BackButton/>
+      <BackButton />
       <h1 className="text-3xl my-4">Delete Book</h1>
-      {loading ? <Spinner/> : ''}
+      {loading ? <Spinner /> : ""}
       <div className="flex flex-col items-center border-2 border-sky-500 rounded-xl w-[600px] p-8 mx-auto">
         <h3 className="text-2xl">Are you sure you want to delete this book?</h3>
-        <button className="p-4 bg-red-700 text-white m-8 w-full" onClick={handleDeleteBook}>
+        <button
+          className="p-4 bg-red-700 text-white m-8 w-full"
+          onClick={handleDeleteBook}
+        >
           Yes, delete it
         </button>
       </div>
     </div>
-  )
+  );
 };
 
 export default DeleteBook;
